732366e083e03b95df5776751753fe6661c8c92e
Bo <bhoo@iastate.edu>
2024-02-22 22:33:50 -0600

    New files and modified and added new lines for part 2

--- activity08/part2/bhoo_Activity08_MoviesFromJSON_FormInput.js
+++ activity08/part2/bhoo_Activity08_MoviesFromJSON_FormInput.js
@@ -4,3 +4,38 @@ Feb 21, 2024
 Activity07
 */
 
+function getInputValue() {
+  fetch("./bhoo_Activity08_MoviesFromJSON.json")
+    .then((response) => response.json())
+    .then((myMovies) => loadMovies(myMovies));
+  let movieName = document.forms["my_form"]["inputMovieName"];
+  let inputMovieName = movieName.value;
+  function loadMovies(myMovies) {
+    var mainContainer = document.getElementById("goodmovies");
+    for (var i = 0; i < myMovies.movies.length; i++) {
+      if (myMovies.movies[i].title === inputMovieName) {
+        // Create a new div
+        var div = document.createElement("div");
+        // Store the current movie in a variable
+        var movie = myMovies.movies[i];
+
+        // Set the inner HTML of the div to the movie details
+        div.innerHTML = `<br>
+          MovieId = ${movie.movieId} <br>
+          Title = ${movie.title} <br>
+          Year = ${movie.year} `;
+
+        // Create a new img element
+        var img = document.createElement("img");
+        // Set the src of the img element to the movie's picture URL
+        img.src = movie.url;
+        // Append the img element to the div
+        div.appendChild(img);
+
+        // Append the div to the main container
+        mainContainer.appendChild(div);
+
+      }
+    }
+  }
+}
