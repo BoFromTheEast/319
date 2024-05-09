7e6a3784a46804deea978d7fe5dbff77abf97890
Ponciano Ramirez <pramirez1347@gmail.com>
2024-05-09 05:23:02 -0500

    all code works

--- finalProject/frontend/src/pokemonInfoPage.js
+++ finalProject/frontend/src/pokemonInfoPage.js
@@ -5,10 +5,11 @@ import setting from './setting.png';
 function PokemonInfoPage() {
   const navigate = useNavigate();
   const { id } = useParams();
+  const [pokemonMoves, setPokemonMoves] = useState([]);
   const [moves, setMoves] = useState([]);
-
-  //fetch first 150 pokemon to show as possible suggestions in search
-  //fetch moves for the specific Pokémon
+  const [moveSearchTerm, setMoveSearchTerm] = useState('');
+  const [filteredMoves, setFilteredMoves] = useState([]);
+  //fetch all moves that a pokemon can learn
   useEffect(() => {
     const userEmail = localStorage.getItem('loginName');
     fetch(`http://localhost:8081/user/${userEmail}/pokemon/${id}/moves`)
@@ -18,7 +19,51 @@ function PokemonInfoPage() {
         console.log(data); // Log the updated moves
       })
       .catch((error) => console.error('Error fetching moves:', error));
-  }, [id]); // Include id and moves in the dependency array
+  }, [id]);
+  //////////
+  useEffect(() => {
+    const filtered = moves.filter((move) =>
+      move.toLowerCase().startsWith(moveSearchTerm.toLowerCase())
+    );
+    setFilteredMoves(filtered);
+  }, [moveSearchTerm, moves]);
+  //////
+  useEffect(() => {
+    const userEmail = localStorage.getItem('loginName');
+    fetch(`http://localhost:8081/user/${userEmail}/pokemon/${id}/movess`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch moves');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Assuming data is an array of move names
+        setPokemonMoves(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching moves:', error);
+      });
+  }, [id]);
+
+  ///////
+  const fetchMoves = () => {
+    const userEmail = localStorage.getItem('loginName');
+    fetch(`http://localhost:8081/user/${userEmail}/pokemon/${id}/movess`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch moves');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Assuming data is an array of move names
+        setPokemonMoves(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching moves:', error);
+      });
+  };
 
   useEffect(() => {
     document.body.style.backgroundColor = 'orange'; // Set background when component mounts
@@ -33,6 +78,61 @@ function PokemonInfoPage() {
   };
   const handleSetting = () => {
     navigate('/Setting');
+  };
+  // Handle adding a move
+  const handleAddMove = async (moveName) => {
+    const userEmail = localStorage.getItem('loginName');
+    const pokemonId = id; // Assuming you have access to the Pokémon ID
+
+    try {
+      const response = await fetch(
+        `http://localhost:8081/user/${userEmail}/pokemon/${pokemonId}/move`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name: moveName }), // Send the move name as 'name' attribute
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to add move');
+      }
+
+      // Reload the list of moves after adding a new move
+      fetchMoves();
+    } catch (error) {
+      console.error('Error adding move:', error);
+    }
+  };
+
+  const handleDeleteMove = async (moveId) => {
+    const userEmail = localStorage.getItem('loginName');
+    const pokemonId = id;
+    try {
+      const response = await fetch(
+        `http://localhost:8081/user/${userEmail}/pokemon/${pokemonId}/move/${moveId}`,
+        {
+          method: 'DELETE',
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to delete move');
+      }
+
+      // Refresh the moves list after deleting a move
+      fetchMoves();
+    } catch (error) {
+      console.error('Error deleting move:', error);
+    }
+  };
+
+  // Handle selecting a move
+  const handleMoveSelect = (moveName) => {
+    setMoveSearchTerm(moveName); // Set the move search term to the selected move name
+    setFilteredMoves(filteredMoves.filter((move) => move !== moveName));
   };
 
   return (
@@ -56,34 +156,52 @@ function PokemonInfoPage() {
         <div className="bg-sky-400 text-white font-bold p-10 rounded-lg shadow-lg flex justify-between items-center">
           <input
             type="text"
-            placeholder="Search Pokemon"
-            className="flex-1 text-black"
+            placeholder="Search Move"
+            className="flex-1 text-black rounded-lg"
             style={{ padding: '8px', fontSize: '16px' }}
-            // value={searchTerm}
-            // onChange={(e) => setSearchTerm(e.target.value)}
+            value={moveSearchTerm}
+            onChange={(e) => setMoveSearchTerm(e.target.value)}
           />
           <button
-            // onClick={() => handleAddPokemon(searchTerm)}
+            onClick={() => handleAddMove(moveSearchTerm)}
             className="bg-orange-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 text-white py-2 px-4 rounded-lg"
           >
             +
           </button>
         </div>
-      </div>
-      {/* Pokemon Moves */}
-      <div className="w-full md:w-1/2 lg:w-1/2 mt-3 bg-orange-400 rounded-lg shadow-lg">
-        <div className="p-5">
-          <h2 className="text-white font-bold text-center mb-3">Moves</h2>
-          <ul className="space-y-2">
-            {moves.map((move, index) => (
-              <li
-                key={index}
-                className="bg-green-500 text-white font-bold p-2 rounded-lg shadow-lg flex justify-between items-center"
+        {moveSearchTerm && (
+          <div className="absolute bg-sky-400 text-white w-full rounded-lg shadow-lg mt-1 overflow-y-auto max-h-60">
+            {filteredMoves.map((move) => (
+              <div
+                key={move}
+                className="text-center p-2 hover:bg-sky-500 cursor-pointer"
+                onClick={() => handleMoveSelect(move)}
               >
                 {move}
-              </li>
+              </div>
             ))}
-          </ul>
+          </div>
+        )}
+      </div>
+      {/* Pokemon Moves */}
+      <div className="w-full md:w-1/2 lg:w-1/2 mt-10 bg-sky-400 rounded-lg shadow-lg">
+        <div className="p-5">
+          {pokemonMoves.map((move) => (
+            <div
+              key={move._id}
+              className="mt-2 bg-green-500 text-white font-bold p-4 rounded-lg shadow-lg flex justify-between items-center"
+            >
+              {/* name of pokemon */}
+              <span className="flex-1 text-center">{move.name}</span>
+              {/* Button to the right */}
+              <button
+                onClick={() => handleDeleteMove(move._id)}
+                className="bg-red-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 text-white py-2 px-4 rounded-lg"
+              >
+                -
+              </button>
+            </div>
+          ))}
         </div>
       </div>
 
